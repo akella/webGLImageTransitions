9a406984cc2eff316f58b9137a6e08bd6b34ce2f
Yuri Artiukh <akella.a@gmail.com>
2019-10-04 14:36:59 +0300

    first transition

---
+++ js/sketch.js
@@ -0,0 +1,154 @@
+
+class Sketch {
+  constructor(opts) {
+    this.scene = new THREE.Scene();
+    this.vertex = `varying vec2 vUv;void main() {vUv = uv;gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );}`;
+    this.fragment = opts.fragment;
+    this.uniforms = opts.uniforms;
+    this.renderer = new THREE.WebGLRenderer();
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1);
+
+    this.container = document.getElementById("slider");
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+
+    this.camera.position.set(0, 0, 2);
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = 0;
+
+    this.paused = true;
+    this.setupResize();
+    this.settings();
+    this.addObjects();
+    this.resize();
+    
+  }
+
+  settings() {
+    let that = this;
+    this.gui = new dat.GUI();
+    this.settings = {progress:0.5};
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+
+    Object.keys(this.uniforms).forEach((item)=> {
+      this.settings[item] = this.uniforms[item].value;
+      this.gui.add(this.settings, item, this.uniforms[item].min, this.uniforms[item].max, 0.01);
+    })
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    
+
+    // image cover
+    this.imageAspect = 1;
+    let a1; let a2;
+    if(this.height/this.width>this.imageAspect) {
+      a1 = (this.width/this.height) * this.imageAspect ;
+      a2 = 1;
+    } else{
+      a1 = 1;
+      a2 = (this.height/this.width) / this.imageAspect;
+    }
+
+    this.material.uniforms.resolution.value.x = this.width;
+    this.material.uniforms.resolution.value.y = this.height;
+    this.material.uniforms.resolution.value.z = a1;
+    this.material.uniforms.resolution.value.w = a2;
+
+
+    // optional - cover with quad
+    const dist  = this.camera.position.z;
+    const height = 1;
+    this.camera.fov = 2*(180/Math.PI)*Math.atan(height/(2*dist));
+
+    // if(w/h>1) {
+    if(this.width/this.height>1){
+      this.plane.scale.x = this.camera.aspect;
+      // this.plane.scale.y = this.camera.aspect;
+    } else{
+      this.plane.scale.y = 1/this.camera.aspect;
+    }
+
+    this.camera.updateProjectionMatrix();
+
+
+  }
+
+  addObjects() {
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable"
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { type: "f", value: 0 },
+        progress: { type: "f", value: 0 },
+        scaleX: { type: "f", value: 40 },
+        scaleY: { type: "f", value: 40 },
+        transition: { type: "f", value: 40 },
+        swipe: { type: "f", value: 0 },
+        width: { type: "f", value: 0 },
+        radius: { type: "f", value: 0 },
+        texture1: { type: "f", value: new THREE.TextureLoader().load('img/1.jpg') },
+        texture2: { type: "f", value: new THREE.TextureLoader().load('img/2.jpg') },
+        displacement: { type: "f", value: new THREE.TextureLoader().load('img/disp3.jpg') },
+        resolution: { type: "v4", value: new THREE.Vector4() },
+      },
+      vertexShader: this.vertex,
+      fragmentShader: this.fragment
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.paused = true;
+  }
+
+  play() {
+    this.paused = false;
+    this.render();
+  }
+
+  render() {
+    if (this.paused) return;
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    this.material.uniforms.progress.value = this.settings.progress;
+
+
+
+    Object.keys(this.uniforms).forEach((item)=> {
+      this.material.uniforms[item].value = this.settings[item];
+    });
+
+
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
